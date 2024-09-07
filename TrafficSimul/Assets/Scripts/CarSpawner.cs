71c8b0c2ccc475c473df0f17567df7d94f666d95
DaBestForSure <125160337+DaBestForSure@users.noreply.github.com>
2024-09-07 14:18:38 -0700

    Added a car spawner and changed the car behavior a lot

--- TrafficSimul/Assets/Scripts/CarSpawner.cs
+++ TrafficSimul/Assets/Scripts/CarSpawner.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject carPrefab;
+    public Transform spawnPoint;
+    public float minSpawnInterval = 2f;
+    public float maxSpawnInterval = 10f;
+    public int maxCarsSpawned = 10;
+    public TrafficLightScript trafficLightScript;
+    private int carsSpawned = 0;
+
     void Start()
     {
-        
+  //      Vector3 spawnPoint = new Vector3(-5324.71f, -1081.593f, -4590.27f);
+        StartCoroutine(SpawnCars());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnCars()
     {
-        
+        while (carsSpawned < maxCarsSpawned)
+        {
+            float spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+            yield return new WaitForSeconds(spawnInterval);
+
+            SpawnCar();
+        }
+    }
+
+    void SpawnCar()
+    {
+        if (carPrefab != null && spawnPoint != null)
+        {
+            GameObject newCar = Instantiate(carPrefab, spawnPoint.position, spawnPoint.rotation);
+            carsSpawned++;
+
+            // Optional: You can add any additional setup for the spawned car here
+            // For example, you might want to set up its CarController component
+            CarController carController = newCar.GetComponent<CarController>();
+            if (carController != null)
+            {
+                carController.trafficLight = trafficLightScript;
+                // Set up any necessary properties of the CarController
+                // For example: carController.trafficLight = someTrafficLight;
+            }
+        }
+        else
+        {
+            Debug.LogError("Car prefab or spawn point is not set in the CarSpawner!");
+        }
     }
 }
